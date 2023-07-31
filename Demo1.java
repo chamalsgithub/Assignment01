7a4029213f91bd6b0f1387388f8dc61f3b9588ee
chamal995 <chamal995@gmail.com>
2023-07-31 23:14:31 +0530

    Re-corrected task 01

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,6 @@
 public class Demo1{
     public static void main(String[] args) {
         double balance = 123456.7890;
-        System.out.printf("Balance: %,.2f%n", balance);
+        System.out.printf("Balance: $%,.2f%n", balance);
     }
 }
